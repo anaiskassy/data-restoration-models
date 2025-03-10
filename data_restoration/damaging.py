f28d24dd0a8f188af2c2235a5a5490820d2df103
anais perrin <perrinana@live.fr>
2025-03-10 19:00:37 +0100

    damaging feature + setup package

--- data_restoration/damaging.py
+++ data_restoration/damaging.py
@@ -1,6 +1,23 @@
-def damaging(X,percent):
+import numpy as np
+
+def damaging(X : np.ndarray ,percent : int,random : bool = True ):
     '''
-    en argumement matrice et un pourcentage de détérioration
+    en argumements matrice et un pourcentage de détérioration
     retourne l'image avec un carré blanc
+    random : position du carré blanc, si False au centre
     '''
-    pass
+    taille = round(np.sqrt(percent/100*X.shape[0]**2),0).astype(int)
+    if random :
+        ind = np.random.randint(0,X.shape[0]-taille)
+    else :
+        ind = round((X.shape[0]-taille)/2,0)
+
+    X_damaged = X.copy()
+
+    for i in range(X.shape[0]) :
+        for j in range(X.shape[1]) :
+            for k in range(3) :
+                if i < ind+taille and i > ind and j > ind and j < ind + taille :
+                    X_damaged[i,j,k] = 255
+
+    return X_damaged
