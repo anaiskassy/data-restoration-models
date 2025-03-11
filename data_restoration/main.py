4ac0d1d74ae6b0dda8e8f2afb5692744775b8c56
anais perrin <perrinana@live.fr>
2025-03-11 14:36:44 +0100

    preprocessing VF

--- data_restoration/main.py
+++ data_restoration/main.py
@@ -1,41 +1,29 @@
 import pandas as pd
 import numpy as np
-
-import tensorflow as tf
-from tensorflow import keras
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import load_img
-
+from pathlib import Path
 from random import sample
+from data_restoration.params import *
+from data_restoration.preprocessing import preprocessed_data
 
-import glob
-from PIL import Image
 
-from preprocessing import preprocessed_data
-
-def load_data(size = 1000, randomized = False):
-
+def load_data(nrows = 1000,chunksize=None):
     """
     Loads image data into a DataFrame.
-
-    Parameters:
-        size (int or str): Number of files to load ('all' for full list).
-        randomized (bool): If True, selects files randomly.
-
     Returns:
         pd.DataFrame: DataFrame containing image arrays.
     """
+    data_processed_path = Path(PATH_PROCESSED_DATA).joinpath("processed_images.csv")
+    if not data_processed_path.is_file() :
+        preprocessed_data()
 
-    images = np.load("{PATH_PROCESSED_DATA}processed_images.npy")
-
-    if size == "all":
-        return images
-
-    size = min(size, len(images))
-
-    if randomized:
-        indices = np.random.choice(len(images), size, replace = False)
-        return images[indices]
-
-    else:
-        return images[:size]
+    if nrows == 'all' :
+        if chunksize == None :
+            images = pd.read_csv(data_processed_path)
+        else :
+            images = pd.read_csv(data_processed_path,chunksize=chunksize)
+    else :
+        if chunksize == None :
+            images = pd.read_csv(data_processed_path,nrows=nrows)
+        else :
+            images = pd.read_csv(data_processed_path,chunksize=chunksize,nrows=nrows)
+    return images
--- data_restoration/preprocessing.py
+++ data_restoration/preprocessing.py
@@ -1,37 +1,40 @@
 import pandas as pd
 import numpy as np
-
-import tensorflow
+from pathlib import Path
 from tensorflow import keras
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
-
 import glob
-
-from params import PATH_PROCESSED_DATA, PATH_RAW_DATA
+from data_restoration.params import *
 
 def preprocessed_data():
 
     '''
-    prend une image en array de taille random et retourne une image en array 300 x 300
+    Process l'ensemble des données en batch et les stocke dans un csv
     '''
-    filelist = glob.glob(f"{PATH_RAW_DATA}/*/*.jpg")
-    print(filelist)
+    # Retrieve `query` data as a DataFrame iterable
+    data_processed_path = Path(PATH_PROCESSED_DATA).joinpath("processed_images.csv")
 
-    df = pd.DataFrame([load_img(file) for file in filelist], columns=['image'])
+    data_processed_exists = data_processed_path.is_file()
+    if not data_processed_exists :
+        chunk_size = 1000
 
-    df['heights'] = df['image'].map(lambda x: x.size[1])
-    df_filtered = df[df["heights"].between(100,500)]
+        filelist = glob.glob(f"{PATH_RAW_DATA}*.jpg")
+        chunk_lists = [filelist[i:i+chunk_size] for i in range(0, len(filelist), chunk_size)]
 
-    df_filtered.reset_index(drop = True, inplace = True)
+        for chunk_id, chunk in enumerate(chunk_lists) :
+            df = pd.DataFrame([load_img(file) for file in chunk], columns=['image'])
+            df.loc['heights'] = df['image'].map(lambda x: x.size[1])
+            df_filtered = df[df["heights"].between(100,500)]
+            df_filtered["image"] = df_filtered["image"].map(lambda x: x.resize((300,300))).map(img_to_array)/255
+            df_final = df_filtered[["image"]]
 
-    df_filtered["image"] = df_filtered["image"].map(lambda x: x.resize((300,300))).map(img_to_array)/255
-
-    df_final = df_filtered[["image"]]
-
-    return df_final
-
-def save_data(df_final):
-
-    np.save(f"{PATH_PROCESSED_DATA}/processed_images.npy", df_final["image"].values)
-    df_final.to_csv(f"{PATH_PROCESSED_DATA}/processed_images.csv", index = False)
+        # Save and append the processed chunk to a local CSV at "data_processed_path"
+            df_final.to_csv(
+                data_processed_path,
+                mode="w" if chunk_id==0 else "a",
+                header=False,
+                index=False,
+                )
+            print(chunk_id)
+    print('Data ready!')
