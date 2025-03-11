cc5b5e7d1baa23eba7d7048288aa9f8caf054fab
anais perrin <perrinana@live.fr>
2025-03-11 09:58:14 +0100

    env data and params

---
+++ data_restoration/params.py
@@ -0,0 +1,6 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+PATH_RAW_DATA = os.environ.get("PATH_RAW_DATA")
+PATH_PROCESSED_DATA = os.environ.get("PATH_PROCESSED_DATA")
