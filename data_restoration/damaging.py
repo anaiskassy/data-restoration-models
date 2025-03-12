d70ec8661959485cacef30c1cd31dfe804196b73
anais perrin <perrinana@live.fr>
2025-03-12 18:11:49 +0100

    amélioration damaging

--- data_restoration/damaging.py
+++ data_restoration/damaging.py
@@ -8,16 +8,18 @@ def damaging(X : np.ndarray ,percent : int,random : bool = True ):
     '''
     taille = round(np.sqrt(percent/100*X.shape[0]**2),0).astype(int)
     if random :
-        ind = np.random.randint(0,X.shape[0]-taille)
+        ind_x = np.random.randint(0,X.shape[0]-taille)
+        ind_y = np.random.randint(0,X.shape[0]-taille)
     else :
-        ind = round((X.shape[0]-taille)/2,0)
+        ind_x = round((X.shape[0]-taille)/2,0)
+        ind_y = ind_x
 
     X_damaged = X.copy()
 
     for i in range(X.shape[0]) :
         for j in range(X.shape[1]) :
             for k in range(3) :
-                if i < ind+taille and i > ind and j > ind and j < ind + taille :
+                if i < ind_x+taille and i >= ind_x and j >= ind_y and j < ind_y + taille :
                     X_damaged[i,j,k] = 255
 
     return X_damaged
