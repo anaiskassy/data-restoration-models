6df1fe6183f603021a8c22219d957eaee8efbf24
anais perrin <perrinana@live.fr>
2025-03-13 17:50:22 +0100

    main created for VM

--- data_restoration/params.py
+++ data_restoration/params.py
@@ -7,3 +7,8 @@ PATH_RAW_HEAD = os.environ.get("PATH_RAW_HEAD")
 PATH_PROCESSED_DATA = os.environ.get("PATH_PROCESSED_DATA")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 PATH_MODELS=os.environ.get("PATH_MODELS")
+
+N_EPOCHS=os.environ.get("N_EPOCHS")
+CHECKPOINT=os.environ.get("CHECKPOINT")
+RELOAD_W=os.environ.get("RELOAD_W")
+BATCH_SIZE=os.environ.get("BATCH_SIZE")
---
+++ main.py
@@ -0,0 +1,25 @@
+from data_restoration.evaluation import *
+from data_restoration.base_models import *
+from data_restoration.load_data import *
+
+# load head cats
+data = load_data_head_small()
+data_train = data[:-100]
+data_test = data[-100:]
+
+# initilization models
+generator,gen_opti,discriminator,disc_opti = init_base_model()
+
+# train base model and save weights
+history_gen, history_disc , predictions_finales , progressive_output = run_base_model(
+    data_train=data_train,
+    generator=generator,
+    gen_opti=gen_opti,
+    discriminator=discriminator,
+    disc_opti=disc_opti,
+    n_epochs=N_EPOCHS,
+    batch_size=BATCH_SIZE,
+    reload_w=RELOAD_W,
+    checkpoint=CHECKPOINT,
+    workbook=False
+)
