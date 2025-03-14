560e8057cd1cc86b2999df7d318a228e4fdc6b93
anais perrin <perrinana@live.fr>
2025-03-14 13:00:19 +0100

    requirements update

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ if int(BASE_MODEL) == 1 :
     data = load_data_head_small(nrows='all',mode='gcloud')
     data_train = data[:-100]
     data_test = data[-100:]
+    print('dataset used = processed_dataset_head_small.h5')
+    print('train = dataset[:-100] --- test = dataset[-100:]')
 
     # initilization models
     generator,gen_opti,discriminator,disc_opti = init_base_model()
+    print('models initialized')
 
     # train base model and save weights
     history_gen, history_disc , predictions_finales , progressive_output = run_base_model(
@@ -24,6 +27,15 @@ if int(BASE_MODEL) == 1 :
         checkpoint=int(CHECKPOINT),
         workbook=False
     )
+    print('models trained')
+
+    # evaluation model
+    gen_loss, disc_loss, generated_images = evaluation_base_model(
+        data_test=data_test,
+        generator=generator,
+        discriminator=discriminator,
+        workbook=False)
+    print('models evaluated')
 
 if int(BASE_MODEL) == 2 :
     print('model pas encore prêt')
