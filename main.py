4240772488a9cc8a5a737ccdd39165688f853e0d
anais perrin <perrinana@live.fr>
2025-03-16 19:45:28 +0100

    try copy from github unet model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data_restoration.evaluation import *
 from data_restoration.base_models import *
 from data_restoration.load_data import *
 from data_restoration.unet_models import *
+from data_restoration.unet_models_copy import *
 from data_restoration.params import *
 
 # ------------------- load data ----------------------#
@@ -103,6 +104,57 @@ if int(MODEL) == 3 :
         reload_w=int(RELOAD_W),
         checkpoint=int(CHECKPOINT),
         workbook=False
+    )
+    print('models trained')
+
+    # # evaluation model
+    gen_loss, disc_loss, generated_images = evaluation_unet_model(
+        data_test=data_test,
+        generator=generator,
+        discriminator=discriminator,
+        workbook=False)
+    print('models evaluated')
+
+
+# ------------------- model U-NET copy ----------------------#
+
+if int(MODEL) == 4 :
+    # initilization models
+    generator = build_reconstruction()
+    gen_opti = recon_optimizer_unet()
+    discriminator = build_adversarial()
+    disc_opti = adv_optimizer_unet()
+    print('models initialized')
+
+    # train base model and save weights
+    # preprocessing data
+    data_train_damaged , expected_output_train = damaging_opti_dataset(data_train)
+    print('data preprocessed')
+
+    # reload weights if needed
+    if int(RELOAD_W) == 1 :
+        local_path_gen = os.path.join(PATH_MODELS,'unet_model','models','generator')
+        local_path_dis = os.path.join(PATH_MODELS,'unet_model','models','discriminator')
+        l_gen = os.listdir(local_path_gen)
+        l_dis = os.listdir(local_path_dis)
+        l_dis.sort()
+        l_gen.sort()
+        path_gen_to_reload = os.path.join(local_path_gen,l_gen[-1])
+        path_dis_to_reload = os.path.join(local_path_dis,l_dis[-1])
+        generator.load_weights(path_gen_to_reload)
+        discriminator.load_weights(path_dis_to_reload)
+
+    # train
+    history_gen, history_disc , predictions , progressive_output = train_unet_model_copy(
+        data=expected_output_train,
+        data_damaged=data_train_damaged,
+        generator=generator,
+        generator_optimizer=gen_opti,
+        discriminator=discriminator,
+        discriminator_optimizer=disc_opti,
+        epochs=int(N_EPOCHS),
+        batch_size=int(BATCH_SIZE),
+        chkpt = int(CHECKPOINT), workbook=False
     )
     print('models trained')
 
